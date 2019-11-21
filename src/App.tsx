6543c848be80fef8a33dbfa8cd8eaca9ba52cf9d
Krunal Patel <krunal.patel@openxcell.info>
2019-11-21 17:50:53 +0530

    apollo graphql package added and called the register and login api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import './App.css';
 
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import Router from './router';
 
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+});
+
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
--- src/containers/pages/register.tsx
+++ src/containers/pages/register.tsx
@@ -1,15 +1,93 @@
-import React, { Component } from 'react'
+import {
+  AbsoluteCol,
+  AbsoluteContainer,
+  AbsoluteRow,
+  CornerCircle,
+  DotContainer,
+  Gradient,
+  Input,
+  LoginBtn,
+  Polygon,
+  Triangle
+} from './login.style'
+import React, {useEffect, useState} from 'react'
+import {emailRegex, passwordRegex} from '../../constant'
 
-import { RouteComponentProps } from 'react-router'
+import {REGISTER_USER} from './login.mutation'
+import {RouteComponentProps} from 'react-router'
+import {useMutation} from '@apollo/react-hooks'
 
-export default class Register extends Component<RouteComponentProps> {
-    state = {}
+const Register: React.FC<RouteComponentProps> = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-    render() {
-        return (
-            <div>
-                Ragister
-            </div>
-        )
-    }
+  const [nameError, setNameError] = useState(true)
+  const [emailError, setEmailError] = useState(true)
+  const [passwordError, setPasswordError] = useState(true)
+
+  const [touched, setTouched] = useState(false)
+
+  useEffect(() => {
+    name.trim().length <= 0 ? setNameError(true) : setNameError(false)
+  }, [name])
+
+  useEffect(() => {
+    passwordRegex.test(password)
+      ? setPasswordError(false)
+      : setPasswordError(true)
+  }, [password])
+
+  useEffect(() => {
+    emailRegex.test(email) ? setEmailError(false) : setEmailError(true)
+  }, [email])
+
+  const isValid = () => !nameError && !emailError && !passwordError
+  const [register, {loading, error}] = useMutation(REGISTER_USER)
+  return (
+    <DotContainer>
+      <Gradient>
+        <CornerCircle />
+        <Polygon />
+        <AbsoluteContainer>
+          <AbsoluteRow>
+            <AbsoluteCol xl={3} lg={3} offset={{xl: 8, lg: 8}}>
+              <Input
+                value={name}
+                onChange={e => setName(e.target.value)}
+                isError={touched && nameError}
+                placeholder='Name'
+              />
+              <Input
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                isError={touched && emailError}
+                placeholder='Email'
+              />
+              <Input
+                type='password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                isError={touched && passwordError}
+                placeholder='Password'
+              />
+              <LoginBtn
+                onClick={() => {
+                  setTouched(true)
+                  if (isValid()) {
+                    register({variables: {email, password, name}})
+                  }
+                }}>
+                {' '}
+                Sign me up!{' '}
+              </LoginBtn>
+            </AbsoluteCol>
+          </AbsoluteRow>
+        </AbsoluteContainer>
+        <Triangle />
+      </Gradient>
+    </DotContainer>
+  )
 }
+
+export default Register
